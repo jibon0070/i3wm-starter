04835718fbf1759616e23354a3846f8008da1cbc
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-21 04:43:57 +0600

    Update config 2024-05-21

--- setup.sh
+++ setup.sh
@@ -9,13 +9,13 @@ fi
 username=$(id -u -n 1000)
 
 apt install nala
-nala install i3 polybar rofi feh alacritty picom variety -y
+nala install i3 i3lock-fancy polybar rofi feh alacritty picom variety -y
 
 if [ ! -d /home/$username/.config ]; then
 	mkdir -p /home/$username/.config
 fi
 
 cp config/* /home/$username/.config -R
-chown $username:$username /home/$username/.config -R
+chown $username:$username /home/$username/.config
 
 exit 0
