ae87824c07a7011cc7cc5f912fef20b600e4b035
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-20 16:36:46 +0600

    feat: Add check for root user in setup script
    Add check in setup.sh to ensure script is run as root user,
    prompting user to run sudo ./install.sh if not.
    This prevents potential issues with insufficient permissions.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+	echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+	exit 1
+fi
+
 exit 0
